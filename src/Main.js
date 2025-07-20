8b81114b1b7129f7d35715b53aa282e49b970423
Haitham-hub-debug <haithama788@gmail.com>
2025-07-20 11:25:08 +0200

    أول رفع للمشروع

---
+++ src/Main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("myBtn");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", () => {
+  result.textContent = "أنت ضغطت الزر! 🎉";
+});
